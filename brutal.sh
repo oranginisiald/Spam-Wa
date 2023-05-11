3469ec157d4638ebff37a2777f729f788cc2c7ac
oranginisiald <130672062+oranginisiald@users.noreply.github.com>
2023-05-11 08:33:52 +0700

    Add files via upload

---
+++ brutal.sh
@@ -0,0 +1,18 @@
+clear
+python3 Spam.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
+python3 Spams.py
